caaf007e58510442107a03c68f47b46bd045f20c
saviaga <saviaga@hotmail.com>
2020-06-18 10:53:09 -0400

    Inorder Iterative

---
+++ InOrderTraversalIterative.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return
+
+        ans = []
+        stack = []
+
+        while root:
+            stack.append(root)
+            root = root.left
+            while root is None and stack:
+                root = stack.pop()
+                ans.append(root.val)
+                root = root.right
+        return ans
