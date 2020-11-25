bc5c1db9d2b9196c2cfbfd73c47d50367d13e8bc
saviaga <saviaga@gmail.com>
2020-11-25 10:16:41 -0500

    working commit - Created using LeetHub

---
+++ n-ary-tree-preorder-traversal.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+​
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return
+        
+        stack = [root]
+        res = []
+        while stack:
+            root = stack.pop()
+            res.append(root.val)
+            for c in reversed(root.children):
+                stack.append(c)
+        return res
+        
