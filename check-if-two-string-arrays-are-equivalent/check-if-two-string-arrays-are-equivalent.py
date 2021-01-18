0989b16a8737a2fd08e1260cca8291911855b747
saviaga <saviaga@gmail.com>
2021-01-18 15:00:31 -0500

    Time: 40 ms (37.79%), Space: 14.2 MB (87.44%) - LeetHub

--- check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
+++ check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.py
@@ -1,10 +1,17 @@
+from itertools import zip_longest
+​
 class Solution:
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
-        concat1 = ''
-        concat2 = ''
-        for elem in word1:
-            concat1 = concat1+elem
         
-        for elem in word2:
-            concat2 = concat2+elem 
-        return concat1 == concat2
+​
+        for char1, char2 in zip_longest(self.string_iter(word1), self.string_iter(word2)):
+                if char1!=char2:
+                    return False
+        return True
+​
+​
+    def string_iter(self,words:List[str])->chr:
+                for word in words:
+                    for char in word:
+                        yield char
+​
