de045fc4e3d5d745aa9e0f38dc018fa8ee8d1592
saviaga <saviaga@gmail.com>
2021-01-20 22:36:15 -0500

    Time: 52 ms (46.14%), Space: 20.2 MB (7.30%) - LeetHub

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -9,19 +9,14 @@ class Solution:
         filter_s = list(filter_s)
         filter_lower = [ch.lower() for ch in filter_s]
         
-        start = 0
+        start= 0
         end = len(filter_lower)-1
         
-        while start< end:
+        while start <=end:
             if filter_lower[start]!=filter_lower[end]:
                 return False
-            else:
-                start+=1
-                end-=1
+            start+=1
+            end-=1
         return True
         
-        
 ​
-            
-        
-        
